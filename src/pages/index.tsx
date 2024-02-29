92f25a4533b7ecaa620bef44e347818b22f886dd
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2024-02-29 15:57:53 -0300

    Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import { styled } from "@/styles";
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  fontSize: 20,
+  color: 'white',
+  padding: '4px 8px',
+  cursor: 'pointer',
+
+  span: {
+    fontWeight: 'bold',
+  },
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>
+      <span>Teste</span>{' '}
+        Enviar
+    </Button>
   );
 }
