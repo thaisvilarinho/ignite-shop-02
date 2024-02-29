d932505f5cd52f21fc85c26dd7008072f87d608a
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2024-02-29 17:42:29 -0300

    Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,29 @@
-import { styled } from "@/styles";
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  fontSize: 20,
-  color: 'white',
-  padding: '4px 8px',
-  cursor: 'pointer',
-
-  span: {
-    fontWeight: 'bold',
-  },
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  }
-})
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta3 from '../assets/camisetas/3.png';
+import camiseta4 from '../assets/camisetas/4.png';
 
 export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>{' '}
-        Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt=""/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt=""/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+    </HomeContainer>
   );
 }
