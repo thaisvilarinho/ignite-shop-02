0eed3db1d1349b3c3d346c02e5ab3c5f91eaaf7f
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2024-03-01 16:47:49 -0300

    Pretech de links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,6 +33,7 @@ export default function Home({ products }: HomeProps) {
             key={product.id}
             href={`/product/${product.id}`}
             legacyBehavior
+            prefetch={false}
             >
             <Product className="keen-slider__slide">
             <Image src={product.imageUrl} width={520} height={480} alt=""/>
