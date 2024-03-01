aa247f41de31b12867ee94140707b061784f97e6
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2024-03-01 15:40:57 -0300

    Carregando dados do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,25 +1,61 @@
+import { stripe } from '@/lib/stripe'
 import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product'
+import { GetStaticProps } from 'next'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+import Stripe from 'stripe'
 
-import camisa1 from '.'
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  }
+}
 
-export default function Product(){
-  const { query } = useRouter()
+export default function Product({ product }: ProductProps){
+
   return (
     <ProductContainer>
       <ImageContainer>
-        
+        <Image src={product.imageUrl} width={520} height={480} alt=''/>
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sint ipsam cum architecto voluptas, totam id veniam velit iure eligendi expedita illum ratione ipsa odit quaerat repellat exercitationem obcaecati nesciunt.</p>
+        <p>
+          {product.description}
+        </p>
 
         <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async ({ params }) => {
+  const productId = params!.id;
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+  const price = product.default_price! as Stripe.Price 
+
+  return {
+    props: {
+      produt: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount! / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1, //1hr
+  }
 }
